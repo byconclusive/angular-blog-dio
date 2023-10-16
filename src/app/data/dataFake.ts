615c8b4224a38c2152f2ae3d321f9a02b3f6faf8
João Filho <joao.casfilho@gmail.com>
2023-10-15 21:45:35 -0300

    Content creat

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake =[
+  {
+    "id":"1",
+    "title": "Thor e seu pode de deus!",
+    "description": "Descubra se você tem algum em comum com o deus do trovão",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2022/05/Doutor-Estranho-2.jpg"
+  },
+  {
+    "id":"2",
+    "title": "novos conteúdos",
+    "description": "ola",
+    "photoCover": ""
+  }
+]
